2af8f09a10b7669b906c409651bd4d81ecea1ff7
cipig <cipianon@gmail.com>
2018-10-21 16:34:28 +0000

    add x11_hash and scrypt to setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     # "xevan_hash" package is required to sync Xuez network.
     # "groestlcoin_hash" package is required to sync Groestlcoin network.
     install_requires=['aiorpcX>=0.8.1,<0.9', 'attrs',
-                      'plyvel', 'pylru', 'aiohttp >= 2', 'uvloop'],
+                      'plyvel', 'pylru', 'aiohttp >= 2', 'uvloop', 'x11_hash', 'scrypt'],
     packages=setuptools.find_packages(include=('electrumx*',)),
     description='ElectrumX Server',
     author='Neil Booth',
